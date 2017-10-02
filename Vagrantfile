88c2a29db0e2908345e2803c6c4ea37186cc73f5
kilroy396 <andrew@boistout.net>
2017-10-02 13:49:36 +1100

    created a base setup file

--- Vagrantfile
+++ Vagrantfile
@@ -11,6 +11,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   #config.vm.box = "ubuntu/trusty64"
   config.ssh.insert_key = false
   config.vm.synced_folder ".", "/vagrant", disabled: true
+  config.vm.provision :shell, :path => "env/base-config.sh"
   config.vm.provider :virtualbox do |v|
     v.memory = 256
     v.linked_clone = true
---
+++ env/base-config.sh
@@ -0,0 +1,11 @@
+yum install ntp -y
+sudo mv /etc/localtime /etc/localtime.bak
+sudo ln -s /usr/share/zoneinfo/Australia/ACT /etc/localtime
+systemctl enable ntpd
+systemctl restart ntpd
+sudo sed -i s/^#baseurl/baseurl/g /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -i s/^#baseurl/baseurl/g /etc/yum.repos.d/epel.repo
+sudo sed -i s/^mirrorlist/#mirrorlist/g /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -i s/^mirrorlist/#mirrorlist/g /etc/yum.repos.d/epel.repo
+#sudo sed -i s+192.168.1.199+192.168.1.15+g /etc/yum.repos.d/*
+sudo yum update -y
